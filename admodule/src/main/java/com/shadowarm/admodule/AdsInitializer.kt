d6f3807e6a157dff8c4824634d02ed442c650673
Awais <waheed@apptrick.net>
2026-02-12 01:16:48 +0500

    version01 with admob interstitial,banner,appopen impl in jetpack compose and making it for compose

--- admodule/src/main/java/com/shadowarm/admodule/AdsInitializer.kt
+++ admodule/src/main/java/com/shadowarm/admodule/AdsInitializer.kt
@@ -7,7 +7,7 @@ import com.google.android.gms.ads.MobileAds
 import com.shadowarm.admodule.ads.consent.ConsentManager
 import com.shadowarm.admodule.ads.remote.AdsConfigInitializer
 import kotlinx.coroutines.launch
-
+//
 class AdsInitializer(
     private val consentManager: ConsentManager,
     private val adsConfigInitializer: AdsConfigInitializer
